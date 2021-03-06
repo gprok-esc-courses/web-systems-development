5aa3260247decf0a3dde25efff339b8caa1b1342
gprok <gprok60@gmail.com>
2021-03-06 10:15:59 +0200

    improved error handling

--- php/07-login/v2/login_do.php
+++ php/07-login/v2/login_do.php
@@ -2,30 +2,32 @@
 require_once "config.php";
 $log = array();
 if(isset($_POST['submit'])) {
-    $log['username'] = filter_var($_POST['username'], FILTER_SANITIZE_STRING);
+    $username = filter_var($_POST['username'], FILTER_SANITIZE_STRING);
     $password = filter_var($_POST['password'], FILTER_SANITIZE_STRING);
 
-    if(empty($log['username']) || empty($password)) {
-        $log['error'] = "All values must be completed";
+    if(empty($username) || empty($password)) {
+        $error = "All values must be completed";
     }
     else {
-        $result = $conn->query("SELECT * FROM users WHERE username='{$log['username']}'");
+        $result = $conn->query("SELECT * FROM users WHERE username='{$username}'");
         if($result->rowCount() == 0) {
-            $log['error'] = "Username is wrong";
+            $error = "Username is wrong";
         }
         else {
             $row = $result->fetch(PDO::FETCH_ASSOC);
             if(password_verify( $password, $row['password'])) {
                 // OK. Proceed
-                $_SESSION['username'] = $log['username'];
+                $_SESSION['username'] = $username;
                 header("Location: dashboard.php");
                 die();
             }
             else {
-                $log['error'] = "Password is wrong";
+                $error = "Password is wrong";
             }
         }
     }
+    $log['username'] = $username;
+    $log['error'] = $error;
     $_SESSION['log'] = $log;
     header("Location: login.php");
     die();
--- php/07-login/v2/register_do.php
+++ php/07-login/v2/register_do.php
@@ -2,35 +2,39 @@
 require_once "config.php";
 $log = array();
 if(isset($_POST['submit'])) {
-    $log['username'] = filter_var($_POST['username'], FILTER_SANITIZE_STRING);
-    $log['email'] = filter_var($_POST['email'], FILTER_SANITIZE_EMAIL);
-    $log['name'] = filter_var($_POST['name'], FILTER_SANITIZE_STRING);
+    $username = filter_var($_POST['username'], FILTER_SANITIZE_STRING);
+    $email = filter_var($_POST['email'], FILTER_SANITIZE_EMAIL);
+    $name = filter_var($_POST['name'], FILTER_SANITIZE_STRING);
     $password = filter_var($_POST['password'], FILTER_SANITIZE_STRING);
     $repeat = filter_var($_POST['repeat'], FILTER_SANITIZE_STRING);
 
-    if(empty($log['username']) || empty($log['email']) ||
-        empty($log['name']) || empty($password)) {
-        $log['error'] = "All values must be completed";
+    if(empty($username) || empty($email) ||
+        empty($name) || empty($password)) {
+        $error = "All values must be completed";
     }
     else if($password != $repeat) {
-        $log['error'] = "Password does not much";
+        $error = "Password does not much";
     }
     else {
-        $result = $conn->query("SELECT * FROM users WHERE username='{$log['username']}'");
+        $result = $conn->query("SELECT * FROM users WHERE username='{$username}'");
         if($result->rowCount() > 0) {
-            $log['error'] = "Username already in use";
+            $error = "Username already in use";
         }
         else {
             // OK, register
             $encrypted = password_hash($password, PASSWORD_BCRYPT);
             $query = "INSERT INTO users(username, email, name, password)
-                      VALUES('{$log['username']}', '{$log['email']}', 
-                             '{$log['name']}', '$encrypted')";
+                      VALUES('{$username}', '{$email}', 
+                             '{$name}', '$encrypted')";
             $conn->exec($query);
             header("Location: login.php");
             die();
         }
     }
+    $log['username'] = $username;
+    $log['email'] = $email;
+    $log['name'] = $name;
+    $log['error'] = $error;
     $_SESSION['log'] = $log;
     header("Location: register.php");
     die();
